f8641e5fba18116c989bf9f958aeeca9c78faf27
Jacob Bryan <jtb36688@gmail.com>
2019-02-20 18:51:46 -0500

    Added first migration

--- index.js
+++ index.js
@@ -1,87 +1,98 @@
-const express = require('express')
-const knex = require('knex')
+const express = require("express");
+const knex = require("knex");
 
 const knexConfig = {
-    client: 'sqlite3',
-    connection: {
-      filename: './data/lambda.sqlite3',
-    },
-    useNullAsDefault: true,
-  };
+  client: "sqlite3",
+  connection: {
+    filename: "./data/lambda.sqlite3"
+  },
+  useNullAsDefault: true
+};
 const db = knex(knexConfig);
 
 const server = express();
 server.use(express.json());
 
-server.get('/api/roles', async (req, res) => {
-    try {
-      const roles = await db('roles');
-      res.status(200).json(roles);
-    } catch (error) {
-      res.status(500).json(error);
-    }
-  });
-  
-  server.get('/api/roles/:id', async (req, res) => {
-    try {
-      const role = await db('roles')
+const errors = {
+  "19": "Another record with that value exists"
+};
+
+server.get("/api/cohorts", async (req, res) => {
+  try {
+    const cohorts = await db("cohorts");
+    res.status(200).json(cohorts);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+server.get("/api/cohorts/:id", async (req, res) => {
+  try {
+    const found = await db("cohorts")
+      .where({ id: req.params.id })
+      .first();
+    res.status(200).json(found);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+server.get("/api/cohorts/:id/students", async (req, res) => {
+  const { id } = req.params.id;
+  try {
+    const found = await db("students").where({ cohort_id: id });
+    res.status(200).json(found);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+server.post("/api/cohorts", async (req, res) => {
+  try {
+    const [id] = await db("cohorts").insert(req.body);
+
+    const addition = await db("cohorts")
+      .where({ id })
+      .first();
+
+    res.status(201).json(addition);
+  } catch (error) {
+    const message = errors[error.errno] || "We ran into an error";
+    res.status(500).json({ message, error });
+  }
+});
+
+server.put("/api/cohorts/:id", async (req, res) => {
+  try {
+    const count = await db("cohorts")
+      .where({ id: req.params.id })
+      .update(req.body);
+
+    if (count > 0) {
+      const updated = await db("cohorts")
         .where({ id: req.params.id })
         .first();
-      res.status(200).json(role);
-    } catch (error) {
-      res.status(500).json(error);
+
+      res.status(200).json(updated);
+    } else {
+      res.status(404).json({ message: "Records not found" });
     }
-  });
-  
-  const errors = {
-    '19': 'Another record with that value exists',
-  };
-  
-  server.post('/api/roles', async (req, res) => {
-    try {
-      const [id] = await db('roles').insert(req.body);
-  
-      const role = await db('roles')
-        .where({ id })
-        .first();
-  
-      res.status(201).json(role);
-    } catch (error) {
-      const message = errors[error.errno] || 'We ran into an error';
-      res.status(500).json({ message, error });
+  } catch (error) {}
+});
+
+server.delete("/api/cohorts/:id", async (req, res) => {
+  try {
+    const count = await db("cohorts")
+      .where({ id: req.params.id })
+      .del();
+
+    if (count > 0) {
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: "Records not found" });
     }
-  });
-  server.put('/api/roles/:id', async (req, res) => {
-    try {
-      const count = await db('roles')
-        .where({ id: req.params.id })
-        .update(req.body);
-  
-      if (count > 0) {
-        const role = await db('roles')
-          .where({ id: req.params.id })
-          .first();
-  
-        res.status(200).json(role);
-      } else {
-        res.status(404).json({ message: 'Records not found' });
-      }
-    } catch (error) {}
-  });
-  
-  server.delete('/api/roles/:id', async (req, res) => {
-    try {
-      const count = await db('roles')
-        .where({ id: req.params.id })
-        .del();
-  
-      if (count > 0) {
-        res.status(204).end();
-      } else {
-        res.status(404).json({ message: 'Records not found' });
-      }
-    } catch (error) {}
-  });
-  
-  const port = process.env.PORT || 5000;
-  server.listen(port, () => console.log(`\nrunning on ${port}\n`));
+  } catch (error) {}
+});
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`\nrunning on ${port}\n`));
